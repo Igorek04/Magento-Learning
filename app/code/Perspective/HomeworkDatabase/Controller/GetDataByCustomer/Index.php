5f77d82ec1e7ae4aae1a2ca45fbf960893bf7972
Igorek04 <burlik.igor2016@gmail.com>
2025-05-01 07:43:58 +0000

    Theme 8(Homework 7-9): Get Data By Title Filter(7) | Get Full Consultation Cost(8) | Get Data By Customer and Datetime(9)

---
+++ app/code/Perspective/HomeworkDatabase/Controller/GetDataByCustomer/Index.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Perspective\HomeworkDatabase\Controller\GetDataByCustomer;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Index implements ActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $pageFactory;
+
+    /**
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * Execute controller action.
+     *
+     * @return ResultInterface
+     */
+    public function execute(): ResultInterface
+    {
+        return $this->pageFactory->create();
+    }
+}

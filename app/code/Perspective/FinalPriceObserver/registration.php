dac0abd5d206e51a9757e240012819c104021d67
Igorek04 <burlik.igor2016@gmail.com>
2025-07-28 08:22:55 +0000

    Theme 16(Shipping Methods) Archive

--- app/code/Perspective/FinalPriceObserver/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Perspective_FinalPriceObserver',
-    __DIR__
-);
